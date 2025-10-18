aa9f32914f237a20523bfad4f1a5445df38b6490
litmus-zhang <abdulsalamlukmon9@gmail.com>
2025-10-18 18:58:58 +0100

    added posthog

--- server/medusa-config.ts
+++ server/medusa-config.ts
@@ -57,21 +57,21 @@ module.exports = defineConfig({
         ],
       },
     },
-    // {
-    //   resolve: "@medusajs/medusa/analytics",
-    //   options: {
-    //     providers: [
-    //       {
-    //         resolve: "@medusajs/analytics-posthog",
-    //         id: "posthog",
-    //         options: {
-    //           posthogEventsKey: process.env.POSTHOG_EVENTS_API_KEY,
-    //           posthogHost: process.env.POSTHOG_HOST,
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
+    {
+      resolve: "@medusajs/medusa/analytics",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/analytics-posthog",
+            id: "posthog",
+            options: {
+              posthogEventsKey: process.env.POSTHOG_EVENTS_API_KEY,
+              posthogHost: process.env.POSTHOG_HOST,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: '@medusajs/medusa/file',
       options: {
